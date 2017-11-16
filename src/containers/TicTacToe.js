71f26a86ea445ace6bd09823968272b7762f366b
Smerth <merth@me.com>
2017-11-16 12:51:47 -0800

    Troubleshooting Canvas Setup, renders the canvas

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,26 +1,33 @@
 import React, {Component} from 'react';
 import {Stage} from 'react-konva';
+import {Board} from '../styled/TicTacToe';
 
 class TicTacToe extends Component {
 
     state = {
-        rows: 3
+        rows: 3,
+        gameState: new Array(9).fill(false),
+        ownMark: 'X',
+        otherMark: 'O',
+        gameOver: false,
+        yourTurn: true,
+        winner: false,
+        win: false
 
     }
 
-    componentWillMount () {
-        // When my comonent mounts - do these things
-        let height = window.innerHeight
-        let width = window.innerwidth
-        let size = (height < width) ? height * .8 : width * .8
-        let rows = this.state.rows
-        let units = size / rows
+    componentWillMount() {
+      let height = window.innerHeight
+      let width = window.innerWidth
+      let size = (height < width) ? height * .8 : width * .8
+      let rows = this.state.rows
+      let unit = size / rows
 
-        this.setState({
-            size,
-            rows,
-            units
-        })
+      this.setState({
+        size,
+        rows,
+        unit
+      })
     }
 
     move = () => {
@@ -40,15 +47,18 @@ class TicTacToe extends Component {
     }
 
     render() {
+      let {
+      size
+    } = this.state
         return (
             <div>
                 <Stage
-                    width={}
-                    height={}
+                    width={size}
+                    height={size}
                 >
                     {/* <Board/> */}
                     {/* <Squares/> */}
-                </Stage>
+              </Stage>
             </div>
         )
     }
