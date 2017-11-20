1c6b6e8b886b9c2d1f3bdd5910d89e1809b6f659
Smerth <merth@me.com>
2017-11-20 11:31:07 -0800

    finished creating user mutation

---
+++ src/mutations/CreateUser.js
@@ -0,0 +1,54 @@
+import Relay from 'react-relay/classic';
+
+export default class CreateUser extends Relay.Mutation {
+
+  getVariables () {
+    return {
+      email: this.props.email,
+      authProvider: {
+        auth0: {
+          idToken: this.props.idToken
+        }
+      },
+
+    }
+  }
+
+  getMutation () {
+    return Relay.QL`mutation{CreateUser}`
+  }
+
+  getFatQuery () {
+    return Relay.QL`
+    fragment on CreateUserPayload {
+      user
+      viewer
+    }
+    `
+  }
+
+  getConfigs () {
+    return [
+      {
+        type: 'RANGE_ADD',
+        parentName: 'viewer',
+        connectionName: 'allUsers',
+        edgeName: 'user',
+        rangeBehaviors: {
+          '': 'append',
+        },
+      },
+      {
+        type: 'REQUIRED_CHILDREN',
+        children: [
+          Relay.QL`
+          fragment on CreateUserPayload {
+            user
+          }
+          `
+        ]
+      }
+    ]
+  }
+
+}
