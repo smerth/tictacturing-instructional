b24a14df979c98ca57edad6b8d0bdfcd6f62900a
Smerth <merth@me.com>
2017-11-23 13:57:08 -0800

    Troubleshooting Auth0 - no fix

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,15 +1,12 @@
-import React, {Component} from 'react';
-import {Stage} from 'react-konva';
-import {Board, Squares} from '../styled/TicTacToe';
-import Relay from 'react-relay/classic';
-import TuringTest from '../styled/TuringTest';
-import CreateGame from '../mutations/CreateGame';
+import React, {Component} from 'react'
+import {Stage} from 'react-konva'
+import {Board, Squares} from '../styled/TicTacToe'
+import Relay from 'react-relay/classic'
+import TuringTest from '../styled/TuringTest'
+import CreateGame from '../mutations/CreateGame'
 
 class TicTacToe extends Component {
 
-  // Define a set a winning combinations of positions
-  // Put in a constructor function to make it available
-  // throughout the app
   constructor(props) {
     super(props)
     this.combos = [
@@ -22,160 +19,130 @@ class TicTacToe extends Component {
       [0,4,8],
       [2,4,6]
     ]
-
   }
 
-    state = {
-        rows: 3,
-        gameState: new Array(9).fill(false),
-        ownMark: 'X',
-        otherMark: 'O',
-        gameOver: false,
-        yourTurn: true,
-        winner: false,
-        win: false
+  state = {
+    rows: 3,
+    gameState: new Array(9).fill(false),
+    ownMark: 'X',
+    otherMark: 'O',
+    gameOver: false,
+    yourTurn: true,
+    winner: false,
+    win: false
+  }
 
-    }
-
-    componentWillMount() {
-      let height = window.innerHeight
-      let width = window.innerWidth
-      let size = (height < width) ? height * .8 : width * .8
-      let rows = this.state.rows
-      let unit = size / rows
-      // generate x and y coordinates needed to draw the grid lines on canvas
-      // using this function makes the lines dependant on rows and unit variables
-      let coordinates = []
-      for (let y = 0; y < rows; y++) {
-          for (let x = 0; x < rows; x++) {
-              coordinates.push([x*unit, y*unit])
-          }
+  componentWillMount() {
+    let height = window.innerHeight
+    let width = window.innerWidth
+    let size = (height < width) ? height * .8 : width * .8
+    let rows = this.state.rows
+    let unit = size / rows
+    let coordinates = []
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < rows; x++) {
+        coordinates.push([x*unit, y*unit])
       }
-
-      this.setState({
-        size,
-        rows,
-        unit,
-        coordinates
-      })
     }
 
-    move = (index, marker) => {
-      // wrap the entire move function in setState
-      // because the state is where we will hold all the info about the game
-        this.setState( (prevState, props) => {
-          // this is the state prior to this move function executing
-          let {gameState, yourTurn, gameOver, winner} = prevState;
-          yourTurn = !yourTurn;
-          // add the move to the gameState array
-          // splice takes the index of the array item to be modified,
-          //the number of arrays to modifiy
-          //and what to place in that array position
-          gameState.splice(index, 1, marker);
-          // check if there is a win
-          let foundWin = this.winChecker(gameState)
-          if(foundWin) {
-            winner = gameState[foundWin[0]];
-          }
-          if(foundWin || !gameState.includes(false)) {
-            gameOver = true;
-          }
-          if(!yourTurn && !gameOver) {
-            this.makeAiMove(gameState)
-          }
+    this.setState({
+      size,
+      rows,
+      unit,
+      coordinates
+    })
+  }
 
-          // this is the new state we return from the function
-          return {
-            gameState,
-            yourTurn,
-            gameOver,
-            win: foundWin || false,
-            winner
-          }
-        })
-    }
-
-    makeAiMove = (gameState) => {
-        // we are just trying to get to mvp (minimum viable product)
-        // so we will just place a mark in at an empty positions
-        // instead of developing or calling an ai api
-        let otherMark = this.state.otherMark
-        let openSquares = []
-        // build an array of the indexes of empty squares
-        gameState.forEach((square, index) => {
-          // if the square is empty
-          if(!square) {
-            openSquares.push(index)
-          }
-        })
-        let aiMove = openSquares[this.random(0, openSquares.length)]
-        // introduce a delay to computer's move cause its thinking
-        setTimeout(() => {
-          this.move(aiMove,otherMark)
-        }, 1000)
-
-
-    }
-
-    turingTest = () => {
-        if(this.state.gameOver) {
-          return(
-            <TuringTest
-              recordGame={this.recordGame}
-            />
-          )
+  move = (index, marker) => {
+    this.setState( (prevState, prop) => {
+        let {gameState, yourTurn, gameOver, winner} = prevState
+        yourTurn = !yourTurn
+        gameState.splice(index, 1, marker)
+        let foundWin = this.winChecker(gameState)
+        if (foundWin) {
+          winner = gameState[foundWin[0]]
         }
-    }
+        if (foundWin || !gameState.includes(false)) {
+          gameOver = true
+        }
+        if (!yourTurn && !gameOver) {
+          this.makeAiMove(gameState)
+        }
+        return {
+          gameState,
+          yourTurn,
+          gameOver,
+          win: foundWin || false,
+          winner
+        }
+    })
+  }
 
-    // given a min and max returns a round random number between the two
-    random = (min, max) => {
-      // ensure min is a round number
-      min = Math.ceil(min)
-      // ensure max is a round number
-      max = Math.floor(max)
-      return Math.floor(Math.random() * (max-min)) + min
-    }
-
-    // Compares the values in the set of arrays in gameState
-    // with the values in set of the combos arrays
-    // return a boolean true if a gameState array matches a combo array
-    winChecker = (gameState) => {
-      let combos = this.combos
-      return combos.find((combo) => {
-        let [a,b,c] = combo
-        return (gameState[a] === gameState[b] && gameState[a] === gameState[c] && gameState[a])
-      })
-
-    }
-
-    recordGame = (guess) => {
-      let { user } = this.props.viewer
-      let { relay } = this.props
-      let { winner, ownMark } = this.state
-      if( user ) {
-        let winnerId = ( winner === ownMark) ? user.id : undefined
-        let guessCorrect = ( guess === 'ROBOT') ? true : false
-        relay.commitUpdate(
-          new CreateGame({
-            user,
-            winnerId,
-            guess,
-            guessCorrect
-          })
-        )
+  makeAiMove = (gameState) => {
+    let otherMark = this.state.otherMark
+    let openSquares = []
+    gameState.forEach( (square, index) => {
+      if(!square) {
+        openSquares.push(index)
       }
-      this.setState({
-        gameState: new Array(9).fill(false),
-        gameOver:false,
-        yourTurn: true,
-        winner: false,
-        win: false
-      })
-     }
+    })
+    let aiMove = openSquares[this.random(0, openSquares.length)]
+    setTimeout(()=>{
+      this.move(aiMove,otherMark)
+    }, 1000)
+  }
 
-    render() {
-      // Call state variables into the render function
-      let {
+  random = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max-min)) + min
+  }
+
+  winChecker = (gameState) => {
+    let combos = this.combos
+    return combos.find( (combo) => {
+      let [a,b,c] = combo
+      return (gameState[a] === gameState[b] && gameState[a] === gameState[c] && gameState[a])
+    })
+  }
+
+  turingTest = () => {
+    if (this.state.gameOver) {
+      return (
+        <TuringTest
+          recordGame={this.recordGame}
+        />
+      )
+    }
+  }
+
+  recordGame = (guess) => {
+    let { user } = this.props.viewer
+    let { relay } = this.props
+    let { winner, ownMark} = this.state
+    if (user) {
+      let winnerId = (winner === ownMark) ? user.id : undefined
+      let guessCorrect = (guess === 'ROBOT') ? true : false
+      relay.commitUpdate(
+        new CreateGame({
+          user,
+          winnerId,
+          guess,
+          guessCorrect
+        })
+      )
+    }
+    this.setState({
+      gameState: new Array(9).fill(false),
+      gameOver: false,
+      yourTurn: true,
+      winner: false,
+      win: false
+    })
+  }
+
+  render() {
+    let {
       size,
       unit,
       rows,
@@ -186,44 +153,44 @@ class TicTacToe extends Component {
       yourTurn,
       ownMark
     } = this.state
-        return (
-            <div>
-              <Stage
-                width={size}
-                height={size}
-              >
-                <Board
-                  unit={unit}
-                  rows={rows}
-                  size={size}
-                />
-                <Squares
-                  unit={unit}
-                  coordinates={coordinates}
-                  gameState={gameState}
-                  win={win}
-                  gameOver={gameOver}
-                  yourTurn={yourTurn}
-                  ownMark={ownMark}
-                  move={this.move}
-                />
-              </Stage>
-              {this.turingTest()}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Stage
+          width={size}
+          height={size}
+        >
+          <Board
+            unit={unit}
+            rows={rows}
+            size={size}
+          />
+          <Squares
+            unit={unit}
+            coordinates={coordinates}
+            gameState={gameState}
+            win={win}
+            gameOver={gameOver}
+            yourTurn={yourTurn}
+            ownMark={ownMark}
+            move={this.move}
+          />
+        </Stage>
+        {this.turingTest()}
+      </div>
+    )
+  }
 }
 
 export default Relay.createContainer(
-    TicTacToe, {
-        fragments: {
-            viewer: () => Relay.QL`
-            fragment on Viewer {
-                user {
-                    id
-                }
-            }
-            `,
+  TicTacToe, {
+    fragments: {
+      viewer: () => Relay.QL`
+        fragment on Viewer {
+          user {
+            id
+          }
         }
+      `,
     }
+  }
 )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import {Route, IndexRoute} from 'react-router';
-import Template from '../containers/Template';
-import TicTacToe from '../containers/TicTacToe';
-import Profile from '../containers/Profile';
-import Relay from 'react-relay/classic';
-import auth from '../utils/auth';
+import React from 'react'
+import {Route, IndexRoute} from 'react-router'
+import Template from '../containers/Template'
+import TicTacToe from '../containers/TicTacToe'
+import Profile from '../containers/Profile'
+import Relay from 'react-relay/classic'
+import auth from '../utils/auth'
 
 const ViewerQueries = {
   viewer: () => Relay.QL`query { viewer }`
 }
 
-const createRoutes = () => {
-    return (
-        <Route
-          path='/'
-          component={Template}
-          queries={ViewerQueries}
-          auth={auth}
-        >
-          <IndexRoute
-            component={TicTacToe}
-            queries={ViewerQueries}
-          />
-          <Route
-            path='/profile'
-            component={Profile}
-            queries={ViewerQueries}
-            />
-        </Route>
-    )
+const userOnly = (nextState, replace) => {
+  if (!auth.getToken()) {
+    replace('/')
+  }
 }
 
-const Routes = createRoutes();
+const createRoutes = () => {
+  return (
+    <Route
+      path='/'
+      component={Template}
+      queries={ViewerQueries}
+      auth={auth}
+    >
+      <IndexRoute
+        component={TicTacToe}
+        queries={ViewerQueries}
+      />
+      <Route
+        path={'/profile'}
+        component={Profile}
+        queries={ViewerQueries}
+        onEnter={userOnly}
+      />
+    </Route>
+  )
+}
 
-export default Routes;
+const Routes = createRoutes()
+
+export default Routes
